e26bb70181ecca219f5007f7b8d6bce4dc5e41d5
lipton-web <cpt.lipton@gmail.com>
2021-10-26 03:37:03 +0900

    final blog

--- src/App.js
+++ src/App.js
@@ -5,15 +5,11 @@ import './App.css';
 function App() {
 
   let [title, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
-  let [like, setLike] = useState(0);
+  let [like, setLike] = useState([0, 0, 0]);
 
   let [modal, setModal] = useState(false);
 
-  let array = [2,3,4];
-
-  array.map(function(){
-
-  });
+  let [input, setInput] = useState('');
 
   // 1. 일단 기존 state 카피본 만들고
   // 2. 카피본에 수정사항 반영하고
@@ -24,6 +20,32 @@ function App() {
     setTitle(newArray);
   }
 
+  // 좋아요 숫자 개별 카운트 기능
+  function newlike(idx) {
+    let newlike = [...like];
+    newlike[idx] += 1 // newlike[idx]++
+    console.log(newlike)
+    setLike(newlike);
+  }
+
+  // map 관련 코딩내용
+  let array = [2,3,4];
+
+  let mapArray = array.map(function(a){
+    return a * 2
+  });
+
+  function forui() {
+    let array = [];
+    for (var i=0; i<3; i++){
+      array.push(<div key={i}>안녕</div>);
+    }
+    return array
+  }
+
+  //내가 방금 누른 제목 넘버
+  let [selectTitle, setSelectTitle] = useState(0);
+
   return (
     <div className="App">
       <div className="black-nav">
@@ -33,41 +55,52 @@ function App() {
       {/* 버튼 누르면 타이틀 바뀜 */}
       <button onClick={changeTitle}>버튼</button>
 
-      <div className="list">
-        <h3>{ title[0] } <span onClick={ () => { setLike(like + 1) }}>👍</span> {like} </h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
+      {
+        title.map((title, i) => { //반복문 돌 때마다 i는 1씩 증가
+          return (
+            <div className="list" key={i}>
+              <h3 onClick={ () => { setSelectTitle(i) } }> { title } <span onClick={ () => { newlike(i) }}>👍</span> {like[i]} </h3>
+              <p>2월 18일 발행</p>
+              <hr/>
+            </div>
+          )
+        })
+      }
 
-      <div className="list">
-        <h3>{ title[1] }</h3>
-        <p>2월 18일 발행</p>
-        <hr/>
-      </div>
+      <div className="publish">
+        <input onChange={ (e)=>{ setInput(e.target.value) } } />
+        <button onClick={ ()=>{ 
+          let addTitle = [...title]; // 직접 수정하면 안되고 사본을 만들어서 수정해 줘야 한다.
+          addTitle.unshift(input); //array 맨 앞에 자료 추가해 줌
+          setTitle(addTitle)
 
-      <div className="list">
-        <h3 onClick={()=>{setModal(true)}}> { title[2] } </h3>
-        <p>2월 19일 발행</p>
-        <hr/>
+          let addLike = [...like]; //좋아요 리스트 추가
+          addLike.unshift(0);
+          setLike(addLike);
+         }}>저장</button>
       </div>
+   
+ 
+
 
       {/* 모달 토글 버튼 */}
       <button onClick={ ()=>{ setModal(!modal)}}>버튼</button>
       {
-        modal === true ? <Modal /> : null
+        modal === true ? <Modal title={title} selectTitle={selectTitle} /> : null  // title의 값을 title로 자식에게 넘겨 줌
       }
       
-
+      {forui()}
     </div>
   );
 }
 
 export default App;
 
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
-      <h2>제목</h2>
+      {/* 부모 컴포넌트의 title값을 받아 와서 사용 */}
+      <h2>제목 { props.title[props.selectTitle] }</h2> 
       <p>날짜</p>
       <p>상세내용</p>
     </div>
